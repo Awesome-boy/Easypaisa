8c1dee96048243a54b4e25da076b31c7948d03c9
Anna Wang <wangzhu@coshine.com>
2019-10-18 09:26:28 +0800

    update for the configure file.

--- app/src/main/java/sdkdemo/kx/come/easypaisa/MainActivity.java
+++ app/src/main/java/sdkdemo/kx/come/easypaisa/MainActivity.java
@@ -1,7 +1,8 @@
 package sdkdemo.kx.come.easypaisa;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
